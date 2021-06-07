4301b9a3568021fd61df63e455af8fe2aa29431f
DESKTOP-IKO3LOH\Admin <vansondtvt29@gmail.com>
2021-06-07 14:14:21 +0700

    tunning push application

--- app/src/main/java/com/example/ledmatrix/MainActivity.kt
+++ app/src/main/java/com/example/ledmatrix/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.ledmatrix
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.advertise.AdvertiseActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_scan_devices)
+//        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        val intent = Intent(this, AdvertiseActivity::class.java)
+        startActivity(intent)
     }
 }
